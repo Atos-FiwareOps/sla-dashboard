82796298bfa54ed26a4b82c74c85ee9bd96b24c6
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-06-29 14:42:12 +0200

    Added parametrization for callback url

--- bin/autoconfigure.sh
+++ bin/autoconfigure.sh
@@ -5,6 +5,7 @@ cp sladashboard/settings.py.sample sladashboard/settings.py
 sed 's/{sla_manager_url}/'$sla_manager_url'/g' sladashboard/settings.py -i
 sed 's/{sla_manager_user}/'$sla_manager_user'/g' sladashboard/settings.py -i
 sed 's/{sla_manager_password}/'$sla_manager_password'/g' sladashboard/settings.py -i
+sed 's/{sla_gui_host}/'$sla_gui_host'/g' sladashboard/settings.py -i
 
 echo "Autoconfiguring executed successfully"
 
