9020a9bab678a8b14bc50aaee5859c2118712fc7
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-06-29 14:57:17 +0200

    Added parametrization for client id and secret

--- bin/autoconfigure.sh
+++ bin/autoconfigure.sh
@@ -6,6 +6,8 @@ sed 's/{sla_manager_url}/'$sla_manager_url'/g' sladashboard/settings.py -i
 sed 's/{sla_manager_user}/'$sla_manager_user'/g' sladashboard/settings.py -i
 sed 's/{sla_manager_password}/'$sla_manager_password'/g' sladashboard/settings.py -i
 sed 's/{sla_gui_host}/'$sla_gui_host'/g' sladashboard/settings.py -i
+sed 's/{idm_client_id}/'$idm_client_id'/g' sladashboard/settings.py -i
+sed 's/{idm_client_secret}/'$idm_client_secret'/g' sladashboard/settings.py -i
 
 echo "Autoconfiguring executed successfully"
 
